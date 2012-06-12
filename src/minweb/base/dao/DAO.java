c333bb4286cf1061f7d9f8ee002f89249ac7ff3f
Leandro Aguiar <leoaguiar456@gmail.com>
2012-06-12 08:07:48 -0300

    Alguns métodos no DAO

--- src/minweb/base/dao/DAO.java
+++ src/minweb/base/dao/DAO.java
@@ -2,6 +2,10 @@ package minweb.base.dao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import minweb.base.modelo.ObjetoBD;
 
@@ -16,5 +20,32 @@ public abstract class DAO<T extends ObjetoBD> {
 	
 	protected EntityManager newEntityManager() {
 		return emf.createEntityManager();
+	}
+	
+	public T getById(int id) {
+		EntityManager em = newEntityManager();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<T> cq = cb.createQuery(targetClass);
+		
+		Root<T> target = cq.from(targetClass);
+		cq.select(target).where(cb.equal(target.get("id"), id));
+		
+		return em.createQuery(cq).getSingleResult();
+	}
+	
+	public void persist(T obj) {
+		EntityManager em = newEntityManager();
+		EntityTransaction et = em.getTransaction();
+		
+		try {
+			et.begin();
+			
+			em.persist(obj);
+			
+			et.commit();
+		} finally {
+			if (et.isActive())
+				et.rollback();
+		}
 	}
 }
