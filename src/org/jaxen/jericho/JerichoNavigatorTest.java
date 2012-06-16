e1d9424f8f60166b5ad223bbe833a0a7eae15054
Leandro Aguiar <leoaguiar456@gmail.com>
2012-06-16 19:54:39 -0300

    Trololo[1]

--- src/org/jaxen/jericho/JerichoNavigatorTest.java
+++ src/org/jaxen/jericho/JerichoNavigatorTest.java
@@ -11,18 +11,28 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
 import org.jaxen.XPath;
 
 public class JerichoNavigatorTest {
 
-	private static String testUrl = "http://www.imdb.com/title/tt1446714/";
-	private static String[] xpaths = new String[] { "/html/body", "//body",
-			"/html/body/../head", "/html/head/title/text()",
-			"//div[@class='articlecontent']", "//div[@class]" };
+	private static String testUrl = "http://www.hagah.com.br/programacao-tv/jsp/default.jspx";
+	private static String[] xpaths = new String[] {
+		"//div[@class='miolo']",
+			
+	};
 
 	public static void main(String[] args) throws Exception {
 		HttpClient client = new DefaultHttpClient();
 		HttpGet get = new HttpGet(testUrl);
+		
+		HttpParams params = get.getParams();
+		
+		params.setIntParameter("uf", 26);
+		params.setIntParameter("operadora", 15);
+		params.setParameter("data", "16/06/2012");
+		params.setParameter("genero", "Filme");
+		
 		HttpResponse response = client.execute(get);
 		HttpEntity entity = response.getEntity();
 
@@ -33,11 +43,14 @@ public class JerichoNavigatorTest {
 			for (String xpath : xpaths) {
 				XPath expr = new JerichoXPath(xpath);
 				Object result = expr.evaluate(doc);
+
 				
 				if (result instanceof Element) {
 					System.out.println(((Element)result).getName());
 				} else if (result instanceof List) {
+					System.out.println(((List<?>)result).size());
 					for (Object obj : (List<?>)result) {
+						System.out.println(obj);
 						if (obj instanceof Element) {
 						} else if (obj instanceof String) {
 						} else if (obj instanceof Number) {
